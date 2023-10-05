c53781267a2d1ebf2a07d6f557cd039befb2139e
alexandre chapleau <alexandrechapleau@hotmail.com>
2023-10-05 09:30:41 -0400

    ajout tour poison + classe obus

---
+++ TP/Obus.py
@@ -0,0 +1,39 @@
+numero_id = 0
+
+
+def prochain_id():
+    global numero_id
+    numero_id += 1
+    return "tour_" + str(numero_id)
+
+
+class Obus():
+    def __init__(self, parent):
+        self.parent = parent
+        self.x = parent.x
+        self.y = parent.y
+        self.alive = True
+        self.collision = False
+        self.id = "obus_" + prochain_id()
+
+
+
+    def voyage_cible(self):
+        cible_x = self.parent.cible_courante.dimensions["x1"]
+        cible_y = self.parent.cible_courante.dimensions["y1"]
+
+        if cible_x > self.x:
+            self.x += 15
+        if cible_x < self.x:
+            self.x -= 15
+        if cible_y > self.y:
+            self.y += 15
+        if cible_y < self.y:
+            self.y -= 15
+
+        if (abs(cible_y - self.y) <= 8
+                and abs(cible_x - self.x) <= 8):
+            self.alive = False
+            self.collision = True
+
+        return self.collision
---
+++ TP/Tour_poison.py
@@ -0,0 +1,59 @@
+import helper as hp
+from Obus import *
+
+
+class Tour_poison():
+    def __init__(self, parent, x, y):
+        self.x = x
+        self.y = y
+        self.parent = parent
+        self.fire_rate = 500
+        self.rayon = 150
+        self.dommage = 20
+        self.temps_poison = 2000
+        self.cible_courante = None
+        self.obus = None
+        self.creeps_trop_loin = []
+        self.distance_avant = {}
+        self.active = True
+
+    def chercher_cible(self, temps):
+        if not len(self.creeps_trop_loin) >= len(self.parent.creeps):
+            if not self.cible_courante:
+                for i in self.parent.creeps:
+                    if i.in_range:
+                        distance = hp.Helper.calcDistance(self.x, self.y, i.dimensions["x1"], i.dimensions["y1"])
+                        if distance <= self.rayon:
+                            self.cible_courante = i
+                            return True
+                        else:
+                            if i.id in self.distance_avant and distance > self.distance_avant[i.id]:
+                                i.in_range = False
+                                self.creeps_trop_loin.append(i)
+                            else:
+                                self.distance_avant[i.id] = distance
+            else:
+                distance = hp.Helper.calcDistance(self.x, self.y, self.cible_courante.dimensions["x1"],
+                                                  self.cible_courante.dimensions["y1"])
+                if distance > self.rayon or self.cible_courante.vie <= 0:
+                    self.cible_courante = None
+                    self.parent.parent.vue.effacer_obus(self.obus)
+                    self.obus = None
+        else: self.active = False
+        return False
+
+    def attaquer_cible(self):
+        if self.cible_courante.vie > 0:
+            if not self.obus:
+                self.obus = Obus(self)
+            else:
+                collision = self.obus.voyage_cible()
+                if collision:
+                    self.cible_courante.vie -= self.dommage
+                    self.parent.parent.vue.effacer_obus(self.obus)
+                    self.obus = None
+
+        if not len(self.creeps_trop_loin) >= len(self.parent.creeps) and self.cible_courante is not None:
+            self.cible_courante = None
+
+
